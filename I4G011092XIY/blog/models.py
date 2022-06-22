902831ed825c58fa59671a1b26f12cfabc81b691
GodsfavourNnaji <godsfavournnaji@gmail.cmm>
2022-06-22 20:35:17 +0100

    Final Commit

--- I4G011092XIY/blog/models.py
+++ I4G011092XIY/blog/models.py
@@ -1,3 +1,12 @@
+from multiprocessing import AuthenticationError
 from django.db import models
+from django.contrib.auth import get_user_model 
+user = get_user_model()
 
 # Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(user, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField
+    Published_date = models.DateTimeField
